116d8ddec7cda81c97f53e4bc7935b8bcc4bdcb3
Rushabh Patel <wsdesignuiux@gmail.com>
2018-12-31 10:37:38 +0530

    Initial commit

---
+++ app/src/main/java/com/example/wolfsoft3/signup_1/SignUp_1.java
@@ -0,0 +1,13 @@
+package com.example.wolfsoft3.signup_1;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SignUp_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_up);
+    }
+}
